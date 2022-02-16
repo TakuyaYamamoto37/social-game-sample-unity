df0cc2ac7c7a97e26b393d4f5c60544112619cb6
TakuyaYamamoto <takuya.yamamoto37@gmail.com>
2022-02-16 13:51:28 +0900

    5-6章キャラクター表示まで終了 p237。 次はキャラクターの売却

---
+++ Assets/CharacterItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterItem : MonoBehaviour
+{
+    public UserCharacterModel userCharacterModel;
+
+    [SerializeField]
+    Image characterAsset;
+
+    [SerializeField]
+    Image typeAsset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MasterCharacterModel masterCharacterModel = MasterCharacter.GetMasterCharacter(userCharacterModel.character_id);
+
+        //Resourcesから画像を読み込む場合
+        Sprite characterSprite = Resources.Load<Sprite>(masterCharacterModel.asset_id);
+        characterAsset.sprite = characterSprite;
+        Sprite typeSprite = Resources.Load<Sprite>("CharacterTypeFrame_" + masterCharacterModel.type);
+        typeAsset.sprite = typeSprite;
+    }
+}
