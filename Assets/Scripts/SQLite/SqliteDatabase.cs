501a14573811405b36f508abec84b8fcf6252f92
TakuyaYamamoto <takuya.yamamoto37@gmail.com>
2022-03-07 12:06:36 +0900

    Sqlite3 マルチバイト文字の対応

--- Assets/Scripts/SQLite/SqliteDatabase.cs
+++ Assets/Scripts/SQLite/SqliteDatabase.cs
@@ -283,8 +283,11 @@ public class SqliteDatabase
 	private IntPtr Prepare (string query)
 	{
 		IntPtr stmHandle;
-        
-		if (sqlite3_prepare_v2 (_connection, query, query.Length, out stmHandle, IntPtr.Zero) != SQLITE_OK) {
+
+		// クエリのバイト数を取得します
+		int byteCount = System.Text.Encoding.UTF8.GetByteCount(query);
+
+		if (sqlite3_prepare_v2 (_connection, query, byteCount, out stmHandle, IntPtr.Zero) != SQLITE_OK) {
 			IntPtr errorMsg = sqlite3_errmsg (_connection);
 			throw new SqliteException (Marshal.PtrToStringAnsi (errorMsg));
 		}
